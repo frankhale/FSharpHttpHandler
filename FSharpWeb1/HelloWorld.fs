257f1c721f5a2116349ff3479fac5b87764912d9
Frank Hale <frankhale@gmail.com>
2015-02-21 23:46:01 -0500

    Hello, F# World!
    - A simple ASP.NET IHttpHandler that when ran will show Hello, F# World!
    in a web browser.

---
+++ FSharpWeb1/HelloWorld.fs
@@ -0,0 +1,12 @@
+﻿namespace FSharpWeb1
+
+open System
+open System.Web
+
+type HelloWorldHandler() =
+  interface IHttpHandler with
+    member this.IsReusable 
+      with get() = false
+    member this.ProcessRequest(context:HttpContext) =
+      context.Request.Headers.Add("text", "html")
+      context.Response.Write("<b>Hello, F# World!</b>")
