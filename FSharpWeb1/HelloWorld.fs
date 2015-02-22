a3c46477e47b8d5d9e8b91047dc97fcb77f65bf4
Frank Hale <frankhale@gmail.com>
2015-02-22 00:04:22 -0500

    I'm such a dork sometimes!
    I often forget how to properly call API's, LOL!

--- FSharpWeb1/HelloWorld.fs
+++ FSharpWeb1/HelloWorld.fs
@@ -8,5 +8,5 @@ type HelloWorldHandler() =
     member this.IsReusable 
       with get() = false
     member this.ProcessRequest(context:HttpContext) =
-      context.Request.Headers.Add("text", "html")
-      context.Response.Write("<b>Hello, F# World!</b>")
+      context.Response.ContentType <- "text/html"
+      context.Response.Write("<hr><b>I love F# OMG!</b>")
