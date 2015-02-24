acf3a51a9baf48ae1d418130093ee9a73b46a6c9
Frank Hale <frankhale@gmail.com>
2015-02-23 22:17:37 -0500

    Make IHttpHandler async

--- FSharpWeb1/HelloWorld.fs
+++ FSharpWeb1/HelloWorld.fs
@@ -3,10 +3,28 @@
 open System
 open System.Web
 
-type HelloWorldHandler() =
-  interface IHttpHandler with
+//type HelloWorldHandler() =
+//  interface IHttpHandler with
+//    member this.IsReusable 
+//      with get() = false
+//    member this.ProcessRequest(context:HttpContext) =
+//      context.Response.ContentType <- "text/html"
+//      context.Response.Write("<b>I love F# OMG!</b>")
+
+type HelloWorld() =
+  static member Handler(ctx:HttpContext) =
+    ctx.Response.ContentType <- "text/html"
+    ctx.Response.Write("<b>I love F# OMG!</b>")
+
+type HelloWorldAsyncIHttpHandler() =
+  let handler = new Action<HttpContext>(HelloWorld.Handler)
+  
+  interface IHttpAsyncHandler with
     member this.IsReusable 
-      with get() = false
-    member this.ProcessRequest(context:HttpContext) =
-      context.Response.ContentType <- "text/html"
-      context.Response.Write("<hr><b>I love F# OMG!</b>")
+      with get() = true
+
+    member this.BeginProcessRequest(ctx, cb, ed) =
+      handler.BeginInvoke(ctx, cb, null)
+
+    member this.EndProcessRequest(result) = ()
+    member this.ProcessRequest(ctx) = ()
