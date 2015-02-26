d6b7c3399fbf00907ed464034f3e5f960388b9f1
Frank Hale <frankhale@gmail.com>
2015-02-26 00:22:46 -0500

    Nothing really, boo!

--- FSharpWeb1/HelloWorld.fs
+++ FSharpWeb1/HelloWorld.fs
@@ -16,47 +16,48 @@ open System.Xml
 //      context.Response.ContentType <- "text/html"
 //      context.Response.Write("<b>I love F# OMG!</b>")
 
+// The feed downloading code was adapted from:
+//  https://github.com/madskristensen/FeedCollector
+
 type HelloWorld() =
   static member private DownloadFeeds() =
     let rss = new SyndicationFeed("ASP.NET News", "News from the ASP.NET and Visual Studio web team", null)
 
     let feeds = Map.ofList [ ("feed:aspnetspotlight", "http://www.asp.net/rss/spotlight");
-                  ("feed:blog.aspnetnews", "http://www.asp.net/rss/content");
-                  ("feed:blog.aspnetteam", "http://blogs.msdn.com/b/webdev/rss.aspx");
-                  ("feed:blog.bradygaster", "http://www.bradygaster.com/feed/rss/");
-                  ("feed:blog.davidfowler", "http://davidfowl.com/rss/");
-                  ("feed:blog.entityframework", "http://blogs.msdn.com/b/adonet/rss.aspx");
-                  ("feed:blog.jeffhandley", "http://feeds.jeffhandley.com/jeffhandley");
-                  ("feed:blog.jeffreyfritz", "http://www.jeffreyfritz.com/feed/");
-                  ("feed:blog.jongalloway", "http://feeds.feedburner.com/jongalloway");
-                  ("feed:blog.madskristensen", "http://feeds.feedburner.com/netSlave");
-                  ("feed:blog.nuget", "http://blog.nuget.org/feeds/atom.xml");
-                  ("feed:blog.sayedhashimi", "http://sedodream.com/SyndicationService.asmx/GetRss");
-                  ("feed:blog.scottguthrie", "https://weblogs.asp.net/scottgu/rss?containerid=13");
-                  ("feed:blog.scotthanselman", "http://feeds.hanselman.com/ScottHanselman");
-                  ("feed:ch9.damianedwards", "http://channel9.msdn.com/Events/Speakers/Damian-Edwards/RSS");
-                  ("feed:ch9.danielroth", "http://channel9.msdn.com/Events/Speakers/daniel-roth/RSS");
-                  ("feed:ch9.davidfowler", "http://channel9.msdn.com/Events/Speakers/david-fowler/RSS");
-                  ("feed:ch9.jongalloway", "http://channel9.msdn.com/Events/Speakers/jon-galloway/RSS");
-                  ("feed:ch9.madskristensen", "http://channel9.msdn.com/Niners/Mads%20Kristensen/RSS");
-                  ("feed:ch9.pranavrastogi", "http://channel9.msdn.com/Events/Speakers/pranav-rastogi/RSS");
-                  ("feed:ch9.sayedhashimi", "http://channel9.msdn.com/Events/Speakers/sayed-hashimi/RSS");
-                  ("feed:ch9.scotthanselman", "http://channel9.msdn.com/Niners/Glucose/RSS");
-                  ("feed:ch9.scotthunter", "http://channel9.msdn.com/Events/Speakers/scott-hunter/RSS");
-                  ("feed:ch9.webcampstv", "http://channel9.msdn.com/Shows/Web+Camps+TV/RSS");
-                  ("feed:youtube.madskristensen", "https://gdata.youtube.com/feeds/base/users/madskvistkristensen/uploads");
-                  ("feed:youtube.sayedhashimi", "https://gdata.youtube.com/feeds/base/users/sayedihashimi/uploads");
-                  ("feed:youtube.scotthanselman", "https://gdata.youtube.com/feeds/base/users/shanselman/uploads") ] 
+                             ("feed:blog.aspnetnews", "http://www.asp.net/rss/content");
+                             ("feed:blog.aspnetteam", "http://blogs.msdn.com/b/webdev/rss.aspx");
+                             ("feed:blog.bradygaster", "http://www.bradygaster.com/feed/rss/");
+                             ("feed:blog.davidfowler", "http://davidfowl.com/rss/");
+                             ("feed:blog.entityframework", "http://blogs.msdn.com/b/adonet/rss.aspx");
+                             ("feed:blog.jeffhandley", "http://feeds.jeffhandley.com/jeffhandley");
+                             ("feed:blog.jeffreyfritz", "http://www.jeffreyfritz.com/feed/");
+                             ("feed:blog.jongalloway", "http://feeds.feedburner.com/jongalloway");
+                             ("feed:blog.madskristensen", "http://feeds.feedburner.com/netSlave");
+                             ("feed:blog.nuget", "http://blog.nuget.org/feeds/atom.xml");
+                             ("feed:blog.sayedhashimi", "http://sedodream.com/SyndicationService.asmx/GetRss");
+                             ("feed:blog.scottguthrie", "https://weblogs.asp.net/scottgu/rss?containerid=13");
+                             ("feed:blog.scotthanselman", "http://feeds.hanselman.com/ScottHanselman");
+                             ("feed:ch9.damianedwards", "http://channel9.msdn.com/Events/Speakers/Damian-Edwards/RSS");
+                             ("feed:ch9.danielroth", "http://channel9.msdn.com/Events/Speakers/daniel-roth/RSS");
+                             ("feed:ch9.davidfowler", "http://channel9.msdn.com/Events/Speakers/david-fowler/RSS");
+                             ("feed:ch9.jongalloway", "http://channel9.msdn.com/Events/Speakers/jon-galloway/RSS");
+                             ("feed:ch9.madskristensen", "http://channel9.msdn.com/Niners/Mads%20Kristensen/RSS");
+                             ("feed:ch9.pranavrastogi", "http://channel9.msdn.com/Events/Speakers/pranav-rastogi/RSS");
+                             ("feed:ch9.sayedhashimi", "http://channel9.msdn.com/Events/Speakers/sayed-hashimi/RSS");
+                             ("feed:ch9.scotthanselman", "http://channel9.msdn.com/Niners/Glucose/RSS");
+                             ("feed:ch9.scotthunter", "http://channel9.msdn.com/Events/Speakers/scott-hunter/RSS");
+                             ("feed:ch9.webcampstv", "http://channel9.msdn.com/Shows/Web+Camps+TV/RSS");
+                             ("feed:youtube.madskristensen", "https://gdata.youtube.com/feeds/base/users/madskvistkristensen/uploads");
+                             ("feed:youtube.sayedhashimi", "https://gdata.youtube.com/feeds/base/users/sayedihashimi/uploads");
+                             ("feed:youtube.scotthanselman", "https://gdata.youtube.com/feeds/base/users/shanselman/uploads") ] 
 
     feeds 
     |> Map.iter(fun key value -> 
-        let feed = HelloWorld.DownloadFeed(value) 
-        if feed.Items.Count() > 0 then
-          rss.Items <- 
-            rss.Items.Union(feed.Items)
-                      .GroupBy(fun i -> i.Title.Text)
-                      .Select(fun i -> i.First())
-                      .OrderByDescending(fun i -> i.PublishDate.Date))
+        let feed = HelloWorld.DownloadFeed(value)
+        rss.Items <- rss.Items.Union(feed.Items)
+                              .GroupBy(fun i -> i.Title.Text)
+                              .Select(fun i -> i.First())
+                              .OrderByDescending(fun i -> i.PublishDate.Date))
 
     rss.Items
 
@@ -64,11 +65,8 @@ type HelloWorld() =
     using(new WebClient()) 
       (fun client -> 
         try
-          let stream = 
-            (async {
-              return! client.OpenReadTaskAsync(url) |> Async.AwaitTask
-            } |> Async.StartAsTask).Result
-          SyndicationFeed.Load(XmlReader.Create(stream))
+          let stream = async { return! client.OpenReadTaskAsync(url) |> Async.AwaitTask } |> Async.StartAsTask
+          SyndicationFeed.Load(XmlReader.Create(stream.Result))
         with
           | _ -> 
               new SyndicationFeed()) 
@@ -77,8 +75,12 @@ type HelloWorld() =
     let feedHtmlBuilder = new StringBuilder()
     let htmlTemplate = "<!DOCTYPE html><html><head><title>ASP.NET News Feeds</title></head><body>{0}</body></html>"
 
+    // It'd be nice if we could do an asynchronous Response.Write but I'm not sure that's possible
+    // We'd need a better way to handle the results while they are downloading. I tried a bunch of 
+    // shit but am still thoroughly confused with F# Async Workflows!
+
     HelloWorld.DownloadFeeds() 
-    |> Seq.iter (fun i -> feedHtmlBuilder.Append(String.Format("<a href='{0}'>{1}</a><br>", i.Links.[0].Uri, i.Title.Text)) |> ignore)
+    |> Seq.iter (fun i -> feedHtmlBuilder.Append(String.Format("<a href='{0}' target='_blank'>{1}</a><br>", i.Links.[0].Uri, i.Title.Text)) |> ignore)
     
     ctx.Response.ContentType <- "text/html"    
     ctx.Response.Write(String.Format(htmlTemplate, feedHtmlBuilder.ToString())) 
